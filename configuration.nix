230b5018d5b2839df8ff727ceb323f9a14a164cf
Mauro Infante <maurux01@gmail.com>
2025-08-19 08:20:57 -0500

    chnge

--- configuration.nix
+++ configuration.nix
@@ -35,6 +35,13 @@
       (emacsPackagesFor emacs).spacemacsPackages.base  # Spacemacs
       onlyoffice-bin    # Added ONLYOFFICE
       masterpdfeditor   # Added Master PDF Editor
+      # Paquetes útiles para i3 y Hyprland
+      dmenu
+      i3status
+      alacritty
+      kitty
+      waybar
+      rofi-wayland
     ];
   };
 
@@ -44,10 +51,41 @@
   # LazyVim (se instalará por ti en Neovim más adelante, como configuración)
   # Puedes clonarlo desde: https://github.com/LazyVim/starter
 
-  # Enable services
+
+  # Habilita X11 y Plasma5
   services.xserver.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  # Habilita i3 como gestor de ventanas X11
+  services.xserver.windowManager.i3.enable = true;
+
+  # Habilita Hyprland (Wayland)
+  programs.hyprland.enable = true;
+
+  # Cambia LightDM por greetd + tuigreet para selección de sesión
+  services.xserver.displayManager.lightdm.enable = false;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "tuigreet --sessions /run/current-system/sw/share/wayland-sessions:/run/current-system/sw/share/xsessions:/run/current-system/sw/share/plasma/sessions";
+        user = "greeter";
+      };
+    };
+  };
+  environment.systemPackages = with pkgs; [
+    tuigreet
+    playerctl
+    pamixer
+    brightnessctl
+    libnotify
+    dunst
+    noto-fonts
+    font-awesome
+    papirus-icon-theme
+    lxappearance
+    qt5ct
+    qt6ct
+  ];
 
   # Enable sound
   sound.enable = true;
