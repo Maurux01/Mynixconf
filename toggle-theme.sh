5b6b7226101d64b226c46bca72be1f61354d18da
Mauro Infante <maurux01@gmail.com>
2025-08-23 00:19:19 -0500

    Add configuration files for i3, Kitty, Picom, Rofi, and Dunst with Catppuccin Mocha theme

--- toggle-theme.sh
+++ toggle-theme.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
-# Toggle between Catppuccin Mocha and Latte for GTK, Qt, and terminal
-
+# Cambia entre Catppuccin Mocha y Latte para GTK, Qt, kitty y rofi
 set -e
-
 USER_CONFIG="$HOME/.config"
 GTKRC="$USER_CONFIG/gtk-3.0/settings.ini"
 QT5CTRC="$USER_CONFIG/qt5ct/qt5ct.conf"
 KITTYRC="$USER_CONFIG/kitty/kitty.conf"
 ROFIRC="$USER_CONFIG/rofi/config.rasi"
-
 if grep -q 'Catppuccin-Mocha' "$GTKRC"; then
-  # Switch to Latte
   sed -i 's/Catppuccin-Mocha.*/Catppuccin-Latte-Standard-Mauve-Light/' "$GTKRC"
   sed -i 's/Catppuccin-Mocha.*/Catppuccin-Latte/' "$QT5CTRC"
   sed -i 's/theme = .*/theme = "Catppuccin-Latte"/' "$KITTYRC"
   sed -i 's/catppuccin-mocha/catppuccin-latte/' "$ROFIRC"
   notify-send "Theme toggled to Catppuccin Latte"
 else
-  # Switch to Mocha
   sed -i 's/Catppuccin-Latte.*/Catppuccin-Mocha-Standard-Mauve-Dark/' "$GTKRC"
   sed -i 's/Catppuccin-Latte.*/Catppuccin-Mocha/' "$QT5CTRC"
   sed -i 's/theme = .*/theme = "Catppuccin-Mocha"/' "$KITTYRC"
